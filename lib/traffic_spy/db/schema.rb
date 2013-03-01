b4587096d4174d335e944c67a8d84ab33f0b702f
novohispano@gmail.com <novohispano@gmail.com>
2013-03-01 11:29:43 -0700

    lala

--- lib/traffic_spy/db/schema.rb
+++ lib/traffic_spy/db/schema.rb
@@ -6,76 +6,76 @@ DB.create_table :actions unless DB.table_exists?(:actions) do
   primary_key :id
   foreign_key :source_id
   foreign_key :url_id
-  DateTime :requested_at
-  Integer :responded_in
+  DateTime    :requested_at
+  Integer     :responded_in
   foreign_key :referrer_id
-  String :request_type
+  String      :request_type
   foreign_key :event_id
   foreign_key :user_agent_id
   foreign_key :resolution_id
   foreign_key :ip_id
-  DateTime :created_at
-  DateTime :updated_at
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :sources unless DB.table_exists?(:sources) do
   primary_key :id
-  String :identifier
-  String :root_url
-  DateTime :created_at
-  DateTime :updated_at
+  String      :identifier
+  String      :root_url
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :urls unless DB.table_exists?(:urls) do
   primary_key :id
-  String :url
-  DateTime :created_at
-  DateTime :updated_at
+  String      :url
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :user_agents unless DB.table_exists?(:user_agents) do
   primary_key :id
-  String :platform #ua.device.platform
-  String :operating_system #ua.device.operating_system
-  String :engine_browser #ua.device.engine.browser
-  String :engine #ua.device.engine
-  DateTime :created_at
-  DateTime :updated_at
+  String      :platform #ua.device.platform
+  String      :operating_system #ua.device.operating_system
+  String      :engine_browser #ua.device.engine.browser
+  String      :engine #ua.device.engine
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :resolutions unless DB.table_exists?(:resolutions) do
   primary_key :id
-  String :width
-  String :height
-  DateTime :created_at
-  DateTime :updated_at
+  String      :width
+  String      :height
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :ips unless DB.table_exists?(:ips) do
   primary_key :id
-  String :address
-  DateTime :created_at
-  DateTime :updated_at
+  String      :address
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :referrers unless DB.table_exists?(:referrers) do
   primary_key :id
-  String :url
-  DateTime :created_at
-  DateTime :updated_at
+  String      :url
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :events unless DB.table_exists?(:events) do
   primary_key :id
-  String :name
-  DateTime :created_at
-  DateTime :updated_at
+  String      :name
+  DateTime    :created_at
+  DateTime    :updated_at
 end
 
 DB.create_table :campaigns unless DB.table_exists?(:campaigns) do
   primary_key :id
-  String :name
+  String      :name
   foreign_key :event_id
-  DateTime :created_at
-  DateTime :updated_at
+  DateTime    :created_at
+  DateTime    :updated_at
 end
